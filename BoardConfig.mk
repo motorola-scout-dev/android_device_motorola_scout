ddf334af89239129758eebad21cc12ab93033858
claxten10 <claxten10@gmail.com>
2025-04-20 19:57:57 +0200

    scout: Define Arch flags
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: Notganesh <chimeports@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,5 +5,11 @@
 
 DEVICE_PATH := device/motorola/scout
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a78
+
 # Inherit the proprietary files
 include vendor/motorola/scout/BoardConfigVendor.mk
