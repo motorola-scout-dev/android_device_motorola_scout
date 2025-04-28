7052582a437665e62b4b87b92193670c28660f70
claxten10 <claxten10@gmail.com>
2025-04-29 01:58:41 +0200

    scout: Configure connectivity stack
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: Notganesh <chimeports@gmail.com>

--- device.mk
+++ device.mk
@@ -102,6 +102,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
+# Connectivity
+PRODUCT_PACKAGES += \
+    init.connectivity.rc
+
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=24m \
