2f8a6dbeba012860f3fe0fc591fea9b09f3af8c7
bengris32 <bengris32@protonmail.ch>
2025-07-04 21:09:41 -0500

    scout: Inherit common mediatek fcm
    Signed-off-by: Notganesh <chimeports@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,7 +57,9 @@ TARGET_SCREEN_DENSITY := 450
 DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
+    $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
 
 # Fastboot
 TARGET_BOARD_FASTBOOT_INFO_FILE := $(DEVICE_PATH)/fastboot-info.txt
