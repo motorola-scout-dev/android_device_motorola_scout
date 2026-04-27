c5a7770bd7f0eb4f4fa4b81423d5f052e188785a
Notganesh <chimeports@gmail.com>
2026-04-27 09:04:27 +0530

    scout: Initial Sepolicy Scout *rebranding From Cybert and Fct *Adapted for Scout
    Co-authored-by: nattolecats <kailua@project2by2.jp>
    Co-authored-by: sarthakroy2002 <sarthakroy2002@gmail.com>
    Co-authored-by: luk1337 <priv.luk@gmail.com>
    Co-authored-by: npjohnson <johnsonnolen@gmail.com>
    Co-authored-by: Mashopy <eliasgheeraert@gmail.com>
    Signed-off-by: Notganesh <chimeports@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -171,6 +171,10 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 
 # Sepolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/basic/non_plat
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Verified Boot
 AB_OTA_PARTITIONS += \
